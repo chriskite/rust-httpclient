38738052d8f8e2758d269f5963ebeea82e70df24
Chris Kite <chris@chriskite.com>
2012-01-25 17:23:11 -0600

    begin implementing post in rust

--- httpclient.rs
+++ httpclient.rs
@@ -1,4 +1,5 @@
 use std;
+import std::map;
 import ctypes::*;
 
 /* Interface with rustcurl */
@@ -15,6 +16,7 @@ type rustcurl_response = {
 #[link_args="-L."]
 native mod rustcurl {
     fn rustcurl_http_get(url: *u8) -> *rustcurl_response;
+    fn rustcurl_http_post(url: *u8, post: *void) -> *rustcurl_response;
 }
 
 native mod curl {
@@ -32,6 +34,18 @@ fn get(url: str) -> response unsafe {
     let resp = rustcurl::rustcurl_http_get(vec::unsafe::to_ptr(url_bytes));
     let body = str::unsafe_from_bytes(vec::unsafe::from_buf((*resp).body.buf, (*resp).body.size));
     let header = str::unsafe_from_bytes(vec::unsafe::from_buf((*resp).header.buf, (*resp).header.size));
+    ret {body: body, header: header}
+}
+
+/* TODO implement this as post */
+fn post(url: str) -> response unsafe {
+    let url_bytes = str::bytes(url);
+
+    let resp = rustcurl::rustcurl_http_get(vec::unsafe::to_ptr(url_bytes));
+    let body = str::unsafe_from_bytes(vec::unsafe::from_buf((*resp).body.buf, (*resp).body.size));
+    let header = str::unsafe_from_bytes(vec::unsafe::from_buf((*resp).header.buf, (*resp).header.size));
+
+
     ret {body: body, header: header}
 }
 
